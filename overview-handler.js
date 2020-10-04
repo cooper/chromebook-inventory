2f709f3d7e990eaff6b8955c42036bd9e5d30da6
Mitchell Cooper <githubspam@rlygd.net>
2020-10-04 10:06:20 -0700

    inject the chart data directly so we don't have to deal with permissions to access the sheets API

--- overview-handler.js
+++ overview-handler.js
@@ -1,3 +1,16 @@
+// create pie chart
 function handleOverview(request, tmpl) {
-    
+
+    // fetch the values
+    let range = ss.getSheets()[0].getRange('C5:D14')
+    let values = range.getDisplayValues()
+
+    // force numeric context of counts
+    for (let i = 0; i < values.length; i++)
+        values[i][1] = parseInt(values[i][1])
+
+    // inject header row
+    values.unshift(['Type', 'Count'])
+
+    tmpl.chartJSON = JSON.stringify(values)
 }
