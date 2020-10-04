18ba68b0b4fa4c201f5bac3650ad6f529232d73e
Mitchell Cooper <githubspam@rlygd.net>
2020-10-04 09:03:35 -0700

    school name and link to spreadsheet, session info, not found page

--- code.js
+++ code.js
@@ -1,18 +1,34 @@
+// fetch the spreadsheet
+let ss = SpreadsheetApp.getActiveSpreadsheet()
+
+// configuration
+let schoolName = ss.getName()
+
+// session info
+let email = Session.getActiveUser().getEmail()
+let user = {
+  email:    email,
+  username: email.substring(0, email.indexOf('@'))
+}
+
 // pages that are allowed
-const pages = ['index', 'status', 'update-status']
+const pages = ['index', 'not-found', 'status', 'update-status']
 let page = 'index'
 
+// handle GET request
 function doGet(request) {
   page = request.parameter.page
 
   // find the page
-  if (!page || !pages.includes(page))
+  if (!page)
     page = 'index' // otherwise show index
+  if (!pages.includes(page))
+    page = 'not-found'
   
   // template parameters
   let tmpl = HtmlService.createTemplateFromFile(page)
   
-  return applyTmplParmas(tmpl).evaluate().setTitle(page + ' | Silvestri Chromebooks')
+  return applyTmplParmas(tmpl).evaluate().setTitle(page + ' | ' + schoolName)
 }
 
 // include template
@@ -22,8 +38,10 @@ function include(filename, params) {
 
 // standard parameters for all templates
 function applyTmplParmas(tmpl) {
+  tmpl.ssUrl = ss.getUrl()
+  tmpl.schoolName = schoolName
   tmpl.page = page
   tmpl.url = ScriptApp.getService().getUrl()
-  tmpl.email = Session.getActiveUser().getEmail()
+  tmpl.user = user
   return tmpl
 }
