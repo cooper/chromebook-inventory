120b0eefdcf468f08d3a65aac7c1872676b2b790
Mitchell Cooper <githubspam@rlygd.net>
2020-10-04 11:44:05 -0700

    available and checked out charts

--- overview-handler.js
+++ overview-handler.js
@@ -18,14 +18,47 @@ function handleOverview(request, tmpl) {
 
     tmpl.overviewChartJSON = JSON.stringify(values)
 
+
+    // count available and checkout
+    let available  = {},
+        checkedOut = {};
+    let inventory = ss.getSheets()[SHEET_INVENTORY].getRange(RANGE_INVENTORY).getDisplayValues()
+
+    inventory.forEach((row) => {
+
+        // only looking for status circulation or circulation cosmetic
+        if (!['CIRCULATION', 'CIRCULATION (COSMETIC)'].includes(row[COL_INVENTORY_STATUS]))
+            return
+
+        let makeModel = row[COL_INVENTORY_MAKE] + ' ' + row[COL_INVENTORY_MODEL]
+
+        // see if checked out and increment proper counter
+        if (row[COL_INVENTORY_CHECKOUT])
+            checkedOut[makeModel] = ++checkedOut[makeModel] || 1
+
+        else
+            available[makeModel] = ++available[makeModel] || 1
+    })
+
     //////////////////////////
     // AVAILABLE PIE CHART ///
     //////////////////////////
 
-    ss.getSheets()[SHEET_INVENTORY]
+    values = Object.entries(available)
+
+    // inject header row
+    values.unshift(['Model', 'Count'])
+
+    tmpl.availableChartJSON = JSON.stringify(values)
 
     /////////////////////////
     // CHECKOUT PIE CHART ///
     /////////////////////////
 
+    values = Object.entries(checkedOut)
+
+    // inject header row
+    values.unshift(['Model', 'Count'])
+
+    tmpl.checkoutChartJSON = JSON.stringify(values)
 }
--- sheets.js
+++ sheets.js
@@ -3,7 +3,8 @@
 // SHEETS
 const   SHEET_OVERVIEW              = 0,
         SHEET_CHANGELOG             = 1,
-        SHEET_INVENTORY             = 2
+        SHEET_LOOKUP_TABLES         = 2,
+        SHEET_INVENTORY             = 3
 
 // OVERVIEW
 const   RANGE_OVERVIEW_CHART        = 'C5:D14'
@@ -20,4 +21,5 @@ const   COL_INVENTORY_BARCODE       = 0,
         COL_INVENTORY_CHECKOUT      = 8,
         COL_INVENTORY_PATRON_ID     = 9,
         COL_INVENTORY_LAST_SYNC     = 10,
-        COL_INVENTORY_LAST_ACTIVITY = 11
+        COL_INVENTORY_LAST_ACTIVITY = 11,
+        RANGE_INVENTORY             = 'A2:M'
